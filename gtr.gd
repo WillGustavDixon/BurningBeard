9bc469a2c8100e132f6c86ec4d9f4620ffea3f26
JQNP <106290019+JQNP@users.noreply.github.com>
2024-03-25 13:44:19 +1100

    Working version of test
    Still buggy, need to fix the suspension and stop it from barrel rolling

--- gtr.gd
+++ gtr.gd
@@ -3,9 +3,13 @@ extends VehicleBody3D
 const MAX_STEER = 0.8
 const ENGINE_POWER = 300
 
+@onready var camera_pivot = $CameraPivot
+@onready var camera_3d = $CameraPivot/Camera3D
+
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-func _process(delta):
-	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
-	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
+func _physics_process(delta):
+	steering = move_toward(steering, Input.get_axis("Right", "Left") * MAX_STEER, delta * 2.5)
+	engine_force = Input.get_axis("Back", "Forward") * ENGINE_POWER
