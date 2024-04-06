45579f72c81a664b35be36f19762ef56d4839c42
JQNP <106290019+JQNP@users.noreply.github.com>
2024-04-06 18:49:40 +1100

    Added camera controls
    included a max pitch/yaw

---
+++ scripts/gtr.gd
@@ -0,0 +1,41 @@
+extends VehicleBody3D
+
+const MAX_STEER = 0.8
+const ENGINE_POWER = 700
+
+@onready var yaw_node = $CameraPivot/CamYaw
+@onready var pitch_node = $CameraPivot/CamYaw/CamPitch
+@onready var camera = $CameraPivot/CamYaw/CamPitch/Camera3D
+@onready var acceleration = 15
+
+var yaw : float = 0
+var pitch : float = 0
+
+var yaw_sensitivity : float = 0.07
+var pitch_sensitivity : float = 0.07
+
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+
+var pitch_max : float = 75
+var pitch_min : float = -55
+
+var yaw_max : float = 90
+var yaw_min : float = -90
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	
+func _physics_process(delta):
+	steering = move_toward(steering, Input.get_axis("Right", "Left") * MAX_STEER, delta * 2.5)
+	engine_force = Input.get_axis("Back", "Forward") * ENGINE_POWER
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	yaw = clamp(yaw, yaw_min, yaw_max)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += -event.relative.y * pitch_sensitivity
