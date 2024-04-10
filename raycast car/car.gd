fb030a18dfd58a96d26687a442bdff9493437012
JQNP <jqnguyenphuoc@gmail.com>
2024-04-10 13:54:11 +1000

    clamped and lerped z axis camera rotation

--- raycast car/car.gd
+++ raycast car/car.gd
@@ -40,14 +40,16 @@ func _process(delta):
 @onready var camera = $CameraPivot/CamYaw/CamPitch/Camera3D
 @onready var acceleration = 15
 
+var camera_rotate_max = 15
+var camera_rotate_min = -15
+
 var yaw : float = 0
 var pitch : float = 0
+var cam_rotation: float = 0
 
 var yaw_sensitivity : float = 0.07
 var pitch_sensitivity : float = 0.07
 
-var yaw_acceleration : float = 15
-var pitch_acceleration : float = 15
 
 var pitch_max : float = 25
 var pitch_min : float = -5
@@ -57,13 +59,14 @@ var yaw_min : float = -25
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
 	
 func _physics_process(delta):
-	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
-	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, acceleration * delta)
+	camera.global_rotation_degrees.z = lerp(camera.global_rotation_degrees.z, cam_rotation, acceleration * delta)
 	pitch = clamp(pitch, pitch_min, pitch_max)
 	yaw = clamp(yaw, yaw_min, yaw_max)
+	cam_rotation = clamp(cam_rotation, camera_rotate_min, camera_rotate_max)
 	
 func _input(event):
 	if event is InputEventMouseMotion:
