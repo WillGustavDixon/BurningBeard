53fc2ce18c186e3f2adfd0bc3e015072f04b92ba
JQNP <106290019+JQNP@users.noreply.github.com>
2024-05-04 16:44:12 +1000

    Changed camera to gimball

--- Scenes/Player/Cart/player_cart.gd
+++ Scenes/Player/Cart/player_cart.gd
@@ -4,9 +4,8 @@ const MAX_STEER = 0.6
 var ENGINE_POWER = 700
 const MAX_BRAKE_FORCE = 5.0 
 
-@onready var yaw_node = $CameraPivot/CamYaw
-@onready var pitch_node = $CameraPivot/CamYaw/CamPitch
-@onready var camera = $CameraPivot/CamYaw/CamPitch/Camera3D
+@onready var gimball = $CameraPivot
+@onready var camera = $CameraPivot/Camera3D
 @onready var acceleration = 15
 
 @export var curCheckpointPos : Vector3
@@ -18,8 +17,8 @@ var yaw_sensitivity : float = 0.07
 var pitch_sensitivity : float = 0.07
 var yaw_acceleration : float = 15
 var pitch_acceleration : float = 15
-var pitch_max : float = 25
-var pitch_min : float = -5
+var pitch_max : float = 20
+var pitch_min : float = -10
 var yaw_max : float = 25
 var yaw_min : float = -25
 
@@ -34,15 +33,13 @@ func _ready():
 	
 func _physics_process(delta):
 	var VELOCITY: Vector3 = get_linear_velocity()
-	
-	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
-	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
 	pitch = clamp(pitch, pitch_min, pitch_max)
 	yaw = clamp(yaw, yaw_min, yaw_max)
-	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, acceleration * delta)
-	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, acceleration * delta)
+	gimball.rotation_degrees.y = lerp(gimball.rotation_degrees.y, yaw, acceleration * delta)
+	gimball.rotation_degrees.x = lerp(gimball.rotation_degrees.x, pitch, acceleration * delta)
 	camera.global_rotation_degrees.z = lerp(camera.global_rotation_degrees.z, cam_rotation, acceleration * delta)
 	cam_rotation = clamp(cam_rotation, camera_rotate_min, camera_rotate_max)
+	
 	if Input.is_action_pressed("Handbrake"):
 		brake_val = 10
 	else:
--- Unsure what to do with this stuff/gtr.gd
+++ Unsure what to do with this stuff/gtr.gd
@@ -3,10 +3,13 @@ extends VehicleBody3D
 const MAX_STEER = 0.6
 const ENGINE_POWER = 800
 
-@onready var yaw_node = $CameraPivot/CamYaw
-@onready var pitch_node = $CameraPivot/CamYaw/CamPitch
-@onready var camera = $CameraPivot/CamYaw/CamPitch/Camera3D
+@onready var gimball = $CameraPivot
+@onready var camera = $CameraPivot/Camera3D
 @onready var acceleration = 15
+
+var camera_rotate_max = 15
+var camera_rotate_min = -15
+var cam_rotation: float = 0
 
 var yaw : float = 0
 var pitch : float = 0
@@ -19,20 +22,14 @@ var pitch_min : float = -5
 var yaw_max : float = 25
 var yaw_min : float = -25
 
-var camera_rotate_max = 15
-var camera_rotate_min = -15
-var cam_rotation: float = 0
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _physics_process(delta):
-	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
-	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
 	pitch = clamp(pitch, pitch_min, pitch_max)
 	yaw = clamp(yaw, yaw_min, yaw_max)
-	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, acceleration * delta)
-	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, acceleration * delta)
+	gimball.rotation_degrees.y = lerp(gimball.rotation_degrees.y, yaw, acceleration * delta)
+	gimball.rotation_degrees.x = lerp(gimball.rotation_degrees.x, pitch, acceleration * delta)
 	camera.global_rotation_degrees.z = lerp(camera.global_rotation_degrees.z, cam_rotation, acceleration * delta)
 	cam_rotation = clamp(cam_rotation, camera_rotate_min, camera_rotate_max)
 	steering = move_toward(steering, Input.get_axis("Right", "Left") * MAX_STEER, delta * 2.5)
@@ -41,4 +38,6 @@ func _physics_process(delta):
 func _input(event):
 	if event is InputEventMouseMotion:
 		yaw += -event.relative.x * yaw_sensitivity
-		pitch += -event.relative.y * pitch_sensitivity
+		pitch += event.relative.y * pitch_sensitivity
+
+
