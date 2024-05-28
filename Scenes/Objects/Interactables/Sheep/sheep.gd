334d97f6f87eb209a4e013bb63bd47c9a172b1ec
JQNP <106290019+JQNP@users.noreply.github.com>
2024-05-29 00:35:29 +1000

    Sheep deletion implemented

--- Scenes/Objects/Interactables/Sheep/sheep.gd
+++ Scenes/Objects/Interactables/Sheep/sheep.gd
@@ -1,6 +1,11 @@
 extends RigidBody3D
 var moving
 
+var sheepPoint: Vector3
+var cartPoint: Vector3
+var cartNode
+
+
 func _ready():
 	## when created, lock the X and Z axes
 	set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, true)
@@ -8,13 +13,17 @@ func _ready():
 	set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_X, true)
 	set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_Z, true)
 	moving = false
+	cartNode = get_tree().get_nodes_in_group("Player")[0]
 
 func _process(delta):
 	if moving:
-		pass
+		sheepPoint = global_position
+		cartPoint = cartNode.global_position
+		if sheepPoint.distance_to(cartPoint) > 500:
+			print("Goodbye Sheep")
+			queue_free()
 
 func _on_body_entered(body):
-	print("Baaaa")
 	if body.is_in_group("Player"):
 		## if it touches the player, unlock the axes.
 		set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, false)
@@ -23,3 +32,6 @@ func _on_body_entered(body):
 		set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_Z, false)
 		moving = true
 		linear_velocity *= 1.2
+
+func _on_cart_cart_position(cartPositionVector):
+	cartPoint = cartPositionVector
--- Scenes/Player/Cart/player_cart.gd
+++ Scenes/Player/Cart/player_cart.gd
@@ -91,7 +91,7 @@ func _physics_process(delta):
 		smoke.amount_ratio = 1
 	else:
 		smoke.amount_ratio = 0.25
-	
+		
 	#Controller Camera
 	yaw += Input.get_action_strength("controllerCameraRight") * con_pitch_sensitivity
 	yaw -= Input.get_action_strength("controllerCameraLeft") * con_yaw_sensitivity
