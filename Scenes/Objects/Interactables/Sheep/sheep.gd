fe3ad0a28f457f42360afabc57718df97053926c
JQNP <106290019+JQNP@users.noreply.github.com>
2024-05-28 23:11:27 +1000

    Sheep done

--- Scenes/Objects/Interactables/Sheep/sheep.gd
+++ Scenes/Objects/Interactables/Sheep/sheep.gd
@@ -3,7 +3,6 @@ var moving
 
 func _ready():
 	## when created, lock the X and Z axes
-	$StandingShape.disabled = false
 	set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, true)
 	set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Z, true)
 	set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_X, true)
@@ -15,13 +14,12 @@ func _process(delta):
 		pass
 
 func _on_body_entered(body):
-	print("I'm Hit!")
+	print("Baaaa")
 	if body.is_in_group("Player"):
 		## if it touches the player, unlock the axes.
 		set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_X, false)
 		set_axis_lock(PhysicsServer3D.BODY_AXIS_LINEAR_Z, false)
 		set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_X, false)
 		set_axis_lock(PhysicsServer3D.BODY_AXIS_ANGULAR_Z, false)
-		$StandingShape.disabled = true
 		moving = true
 		linear_velocity *= 1.2
